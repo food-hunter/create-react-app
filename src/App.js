c3f82cf1b76fcea4d1fe183cc00f2f067799caca
food-hunter <116442078+food-hunter@users.noreply.github.com>
2024-10-31 20:55:24 -0400

    added favicon updated index html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import Food from './components/Food';
+import About from './components/About';
+import Contact from './components/Contact/Contact';
+
+const componentNames = ['Home', 'Food', 'About', 'Contact']
+
+const NavButton = ({ handleClick, componentName }) => {
+  return (
+    <p
+      onClick={() => {
+        handleClick(componentName)
+      }} >
+      {
+        componentName
+      }
+    </p>
+  )
+}
 
 function App() {
+  const [currentComponent, setCurrentComponent] = React.useState('Home')
+
+  const handleClick = (nextComponent) => {
+    console.log(nextComponent)
+    setCurrentComponent(nextComponent)
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Bread
-        </a>
-      </header>
+
+    <div>
+      <div className="topnav">
+        {
+          componentNames.map((componentName) => {
+            return (<NavButton handleClick={handleClick} componentName={componentName} key={componentName} />)
+          })
+        }
+      </div>
+      <div className="App">
+        {
+          currentComponent === 'Home' && <Home />
+        }
+        {
+          currentComponent === 'Food' && <Food />
+        }
+        {
+          currentComponent === 'Contact' && <Contact />
+        }
+        {
+          currentComponent === 'About' && <About />
+        }
+      </div>
     </div>
   );
 }
